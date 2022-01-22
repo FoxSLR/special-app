27b900228735b0556a98a0f69d932e2e43a1256b
Evgenii Philippov <ef.sib@mail.ru>
2022-01-23 00:30:19 +0800

    added some code

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ current_datetime = datetime.now()
 print('Текушая дата и время: ', end = "")
 print(current_datetime)
 
+task = int(input('Сколько еще бодрствуем ?'))
+if task > 3:
+    print('Не выспишься...')
+else:
+    print('Норм')
