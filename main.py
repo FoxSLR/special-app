2aef3acfc2330fe6fbdf444eeca558a7ac36fd22
Evgenii Philippov <ef.sib@mail.ru>
2022-01-25 02:02:52 +0800

    some changes in main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 
 from datetime import datetime
 current_datetime = datetime.now()
-print('Текушая дата и время: ', end = "")
+print('\nТекушая дата и время: ', end = "")
 print(current_datetime)
 
 print('Nothing to report, Master')
-
+print('=========================')
